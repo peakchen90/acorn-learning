6d386c29132ecedb371e7a9f78c8d14b292d3c38
peakchen90 <1558267774@qq.com>
2020-08-22 20:46:30 +0800

    翻译注释

--- acorn/src/tokentype.js
+++ acorn/src/tokentype.js
@@ -3,23 +3,28 @@
 // The assignment of fine-grained, information-carrying type objects
 // allows the tokenizer to store the information it has about a
 // token in a way that is very cheap for the parser to look up.
+// 分配细粒度的，承载信息的类型对象，使令牌生成器可以以对解析器查找非常便宜的方式存储有关令牌的信息。
 
 // All token type variables start with an underscore, to make them
 // easy to recognize.
+// 所有的 token 类型变量都以下划线开始，使它们很好识别
 
 // The `beforeExpr` property is used to disambiguate between regular
 // expressions and divisions. It is set on all token types that can
 // be followed by an expression (thus, a slash after them would be a
 // regular expression).
+// `beforeExpr` 属性用于消除正常表达式和除法之间的歧义。它在所有可以跟随表达式的标记类型上设置（因此，它们后面的斜杠将是正则表达式）。
 //
 // The `startsExpr` property is used to check if the token ends a
 // `yield` expression. It is set on all token types that either can
 // directly start an expression (like a quotation mark) or can
 // continue an expression (like the body of a string).
+// `startsExpr` 属性用于检查令牌是否以 `yield` 表达式结尾。在所有可以直接开始表达式（如引号）或可以继续表达式（如字符串的主体）的令牌类型上设置它。
 //
 // `isLoop` marks a keyword as starting a loop, which is important
 // to know when parsing a label, in order to allow or disallow
 // continue jumps to that label.
+// `isLoop` 将关键字标记为开始循环，这对于解析标签时了解这一点很重要，以便允许或禁止继续跳转到该标签。
 
 export class TokenType {
   constructor(label, conf = {}) {
@@ -81,16 +86,20 @@ export const types = {
   // Operators. These carry several kinds of properties to help the
   // parser use them properly (the presence of these properties is
   // what categorizes them as operators).
+  // 运算符。它们带有多种属性以帮助解析器正确使用它们（这些属性的存在将它们归类为运算符）。
   //
   // `binop`, when present, specifies that this operator is a binary
   // operator, and will refer to its precedence.
+  // `binop`（如果存在）指定此运算符为二进制运算符，并将引用其优先级。
   //
   // `prefix` and `postfix` mark the operator as a prefix or postfix
   // unary operator.
+  // `prefix` 和 `postfix` 将运算符标记为前缀或后缀一元运算符
   //
   // `isAssign` marks all of `=`, `+=`, `-=` etcetera, which act as
   // binary operators with a very low precedence, that should result
   // in AssignmentExpression nodes.
+  // `isAssign` 标记了所有 `=`、`+=`、`+=` 等，它们以非常低的优先级充当二进制运算符，应该会 AssignmentExpression 节点。
 
   eq: new TokenType("=", {beforeExpr: true, isAssign: true}),
   assign: new TokenType("_=", {beforeExpr: true, isAssign: true}),
