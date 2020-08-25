3fe0cb950353be1ae257c95f8550033dc3d972a4
peakchen90 <1558267774@qq.com>
2020-08-25 21:28:13 +0800

    +

--- acorn/test.js
+++ acorn/test.js
@@ -1,23 +1,11 @@
 const acorn = require('./dist/acorn');
 
 const code = `
-const obj = {
-  name: "",
-  say() {
-    console.log(this.name);
-  }
-}
-
-obj
-  ..name = "chen"
-  ..say()
+  const foo = "hello world";
+  export default foo;
 `
 
-const Parser = acorn.Parser.extend(
-  require('./plugins/cascades')
-)
-
-const ast = Parser.parse(code, {
+const ast = acorn.parse(code, {
   sourceType: 'module',
   locations: true
 });
